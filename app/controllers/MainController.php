9f3c4686249f6a95da88724fa587cfdb00dde7f6
wemitwqw <55079623+wemitwqw@users.noreply.github.com>
2022-11-01 02:05:59 +0200

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,16 +12,25 @@ class MainController extends Controller {
     }
 
     public function addItemAction() {
-        $form = 2;
+        // $form = 2;
 
-        $params = [
-            'id' => 2,
-        ];
+        // $params = [
+        //     'id' => 2,
+        // ];
 
-        $db = new Db();
-        $data = $db->column('SELECT id, name FROM items WHERE id = :id', $params);
-        debug($data);
+        // $db = new Db();
+        // $data = $db->column('SELECT id, name FROM items WHERE id = :id', $params);
+        // debug($data);
 
-        echo 'add item controller';
+        if ($_POST) {
+            
+            
+            foreach ($_POST as $key => $value) {
+                echo $key.'='.$value.'<br>';
+            };
+        } else {
+            echo json_encode('Error: POST request with form-data expected, but not received.');
+        }
+        echo json_encode('add item controller');
     }
 }
